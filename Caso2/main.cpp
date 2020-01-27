413f2bc6e9a36f32b40cfd03be6befddcfe2185d
Vandersal Aheram <163239@ids.upchiapas.edu.mx>
2020-01-26 18:41:13 -0600

    Funcion paquete internet 5GB terminada y limite dispositivos 5GB terminado, al igual que la funcion de limite de GB que se usa en ambos casos, terminado

--- Caso2/main.cpp
+++ Caso2/main.cpp
@@ -3,8 +3,19 @@
 
 using namespace std;
 
+//Variables
+int opcionMenuPrincipal;
+int opcionInternet, dispositivosConectados, consumoGB, cantidadUsuarios, sumaDatos;
+int opcionDispo, opcionGB;
+
+
+
 //Constructores de las funciones
 void menuPrincipal();
+void menuInternet();
+void paqueteInternetCasa5GB();
+void limiteDispositivos5GB();
+void limiteGB();
 
 int main() {
     menuPrincipal();
@@ -12,9 +23,135 @@ int main() {
 }
 
 void menuPrincipal(){
-    cout<<"-----        -----"<<endl;
-    cout<<"Bienvenido"<<endl;
-    cout<<""<<endl;
-    cout<<""<<endl;
+
+    cout << "\n-----        -----" << endl;
+    cout << "Bienvenido al cliente para soporte" << endl;
+    cout << "1. Telefonia" << endl;
+    cout << "2. Internet" << endl;
+    cout << "3. Salir" << endl;
+    cout << "Por favor ingresa tu opcion deseada " << endl;
+    cout << "Opcion: ";
+    cin >> opcionMenuPrincipal;
+    cout << "\n";
+    switch (opcionMenuPrincipal){
+        case 1:
+            break;
+        case 2:
+            menuInternet();
+            break;
+        case 3:
+            cout<<"Adios"<<endl;
+        default:
+            cout<<"Error, opcion no valida para el proceso :("<<endl;
+            break;
+    }
 
 }
+
+void menuInternet(){
+    cout<<"\nEl cliente tiene problemas con el internet"<<endl;
+    cout<<"Que paquete tiene el cliente?"<<endl;
+    cout<<"1. Internet en casa (5 Mbps)"<<endl;
+    cout<<"2. Internet en casa (10 Mbps)"<<endl;
+    cout<<"3. Regresar"<<endl;
+    cout<<"Opcion: ";
+    cin>>opcionInternet;
+
+    switch (opcionInternet){
+        case 1:
+            paqueteInternetCasa5GB();
+            break;
+        case 2:
+
+            break;
+        case 3:
+            menuPrincipal();
+            break;
+        default:
+            cout<<"Opcion invalida"<<endl;
+            break;
+    }
+    cout<<"\n";
+
+}
+
+void paqueteInternetCasa5GB(){
+    cout<<"\nConoce cuantos dispositivos se encuentran concectados? (1 = SI y 2 = NO)"<<endl;
+    cout<<"Opcion: ";
+    cin>>opcionDispo;
+    cout<<"\nConoce el consumo de datos? (1 = SI y 2 = NO)"<<endl;
+    cout<<"Opcion: ";
+    cin>>opcionGB;
+
+    if(opcionDispo <0 && opcionGB < 0){
+        cout<<"Datos invalidos"<<endl;
+        menuInternet();
+    }else{
+
+        if(opcionDispo < 0 || opcionGB <0 ){
+            cout<<"Datos invalidos"<<endl;
+            menuInternet();
+        }else{
+            if(opcionDispo == 1){
+                limiteDispositivos5GB();
+            }
+            if(opcionGB == 1){
+                limiteGB();
+            }
+        }
+
+    }
+}//Funcion terminada
+
+void limiteDispositivos5GB(){
+    cout<<"\nCuantos dispositivos se encuentran conectados a la red?"<<endl;
+    cout<<"Opcion: ";
+    cin>>dispositivosConectados;
+
+    if (dispositivosConectados <0){
+        cout<<"Error, opcion no valida"<<endl;
+        menuInternet();
+    }else {
+
+        if(dispositivosConectados < 3){
+            cout<<"El usuario no excede la cantidad de dispositivos"<<endl;
+
+        }else {
+            cout<<"El usuario tiene mas de los dispositivos permitidos "<<endl;
+        }
+    }
+}//Funcion terminada
+
+void limiteGB(){
+    cout<<"De cuanto es tu consumo de GB de datos?"<<endl;
+    cout<<"Consumo: ";
+    cin>>consumoGB;
+
+    if (consumoGB < 0){
+        cout<<"Error, dato invalido"<<endl;
+        menuInternet();
+    }else
+
+    if (consumoGB < 100){
+        cout<<"No has llegado al limite de conexiones permitidas, excelente"<<endl;
+    }
+    else{
+        if(opcionDispo == 2){
+            cout<<"Tu velocidad bajo a 0.5 Mbps"<<endl;
+            cout<<"Por eso tu servicio falla, gracias por contacar al soporte tecnico"<<endl;
+        }else{
+            cout<<"Tu velocidad bajo a 0.5 Mbps"<<endl;
+            //Falta codigo, complementalo
+            cout<<"Por favor, ingresa la cantidad aproximada de datos consumidos por cada usuario"<<endl;
+            for (int i = 1; i <=dispositivosConectados ; ++i) {
+                cout<<"Dispositivo "<<i<<" : ";
+                cin>>cantidadUsuarios;
+                cout<<" "<<endl;
+                sumaDatos = cantidadUsuarios + cantidadUsuarios;
+            }
+            cout<<"Tu consumo total de GB considerando que excedes el limite es de: "<<sumaDatos<<endl;
+            cout<<"Gracias por llamar al soporte"<<endl;
+        }
+
+    }
+}//Funcion terminada
